5c5375a4b17c8aa45b3387706a22fb37833e3fb0
robedj <ed.wassouf@gmail.com>
2022-08-16 21:27:32 -0400

    W.I.P : import do Cliente na interface ClienteRepository

--- src/main/java/br/apiEd/cliente/repository/ClienteRepository.java
+++ src/main/java/br/apiEd/cliente/repository/ClienteRepository.java
@@ -1,5 +1,6 @@
 package br.apiEd.cliente.repository;
 
+import br.apiEd.cliente.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
