db253dc240aa058ef168e9df3cd1c8118d1badb1
robedj <ed.wassouf@gmail.com>
2022-08-16 21:28:26 -0400

    W.I.P : Criação dos metodos ListaCliente, save, buscaId, removebyId,

--- src/main/java/br/apiEd/cliente/service/ClienteService.java
+++ src/main/java/br/apiEd/cliente/service/ClienteService.java
@@ -1,8 +1,11 @@
 package br.apiEd.cliente.service;
 
+import br.apiEd.cliente.entity.Cliente;
 import br.apiEd.cliente.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClienteService {
@@ -13,7 +16,15 @@ public class ClienteService {
     public Cliente salvar(Cliente cliente){
         return clienteRepository.save(cliente);
     }
-
+    public List<Cliente> listaCliente(){
+        return clienteRepository.findAll();
+    }
+    public Optional<Cliente> buscaId(Long id){
+        return clienteRepository.findById(id);
+    }
+    public void removeById(Long id){
+        clienteRepository.deleteById(id);
+    }
 }
 
 
