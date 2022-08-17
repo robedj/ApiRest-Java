bd18d88bd62096b319e3fe903ce5bdbdebed6583
robedj <ed.wassouf@gmail.com>
2022-08-16 21:10:47 -0400

    W.I.P : mplementação do ClienteService, construção dos métodos

---
+++ src/main/java/br/apiEd/cliente/service/ClienteService.java
@@ -0,0 +1,19 @@
+package br.apiEd.cliente.service;
+
+import br.apiEd.cliente.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public Cliente salvar(Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
+}
+
+
